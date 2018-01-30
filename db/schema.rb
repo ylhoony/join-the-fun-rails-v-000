ef288529ca3e310da88ebc72f3f2b589db1dbc19
Hoon Lee <yl.hoony@gmail.com>
2018-01-30 18:24:43 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140212205242) do
-
-  create_table "rides", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20140212204456) do
 
   create_table "taxis", force: :cascade do |t|
     t.datetime "created_at", null: false
